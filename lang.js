d73d80d1aacdfb6948a054758fde964e6d34ced9
Volha <olgasabich@mail.ru>
2025-02-23 21:32:47 +0300

    lang

---
+++ lang.js
@@ -0,0 +1,39 @@
+const langArr = {
+    "season" : {
+        "ru": "СЕЗОН" 
+        "eng": "SEASON" 
+    };
+    "mara" : {
+        "ru": "МАРАФОНЫ" 
+        "eng": "MARATHOS" 
+    };
+    "arch" : {
+        "ru": "АРХИВ" 
+        "eng": "ARCHIVE" 
+    };
+    "s1" : {
+        "ru": "1 сезон" 
+        "eng": "Season 1" 
+    };
+    "s2" : {
+        "ru": "2 сезон" 
+        "eng": "Season 2" 
+    };
+    "s3" : {
+        "ru": "3 сезон" 
+        "eng": "Season 3" 
+    };
+    "s4" : {
+        "ru": "4 сезон" 
+        "eng": "Season 4" 
+    };
+    "s5" : {
+        "ru": "5 сезон" 
+        "eng": "Season 5" 
+    };
+    "s6" : {
+        "ru": "6 сезон" 
+        "eng": "Season 6" 
+    };
+}
+}
